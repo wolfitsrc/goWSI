b8e89d9cd01dd43de51435cf118bdfe06e794e1c
Gavin Hell <gmangavin1@gmail.com>
2023-05-18 06:35:08 -0700

    The first go file / branch for the project

---
+++ main.go
@@ -0,0 +1,15 @@
+// WSI short for Wolf Software Installer
+// This projects idea is to create a simple CLI tool for making a installer for a binary program.
+// The first couple features to be implemented are:
+// 1. Allow for the installer to add path's to the PATH environment variable.
+// 2. Allow for the installer to hold static binary files that get copied to a specified location.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to WSI")
+}
