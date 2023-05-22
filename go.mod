2fbb1d4adb6ddb7ae1a62cad350d1609c2c9ad6b
Gavin Hell <gmangavin1@gmail.com>
2023-05-22 06:26:20 -0700

    Working admin permission

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wolfit-src/goWSI
 
 go 1.20
+
+require golang.org/x/sys v0.8.0
--- main.go
+++ main.go
@@ -9,6 +9,11 @@ package main
 import (
 	"embed"
 	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/sys/windows"
 
 	"github.com/wolfit-src/goWSI/components/gen"
 )
@@ -29,6 +34,39 @@ var Embedded embed.FS
 func main() {
 	err := gen.InstallerExtract(Embedded, EmbFileName, ExportLoc)
 	if err != nil {
-		fmt.Println(err)
+		if checkAdmin() {
+			fmt.Println(err)
+		} else {
+			becomeAdmin()
+		}
 	}
+}
+
+func becomeAdmin() {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		fmt.Println(err)
+		err := gen.InstallerExtract(Embedded, EmbFileName, ExportLoc)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func checkAdmin() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+
+	return err == nil
 }
